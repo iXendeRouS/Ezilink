106e62d0b610a079a59b89416eda0af3b91a0e81
iXendeRouS <aharrison9608@gmail.com>
2025-03-17 22:15:18 +1300

    updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace Ezilink;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "Ezilink";
 
     public const string Description = "Buy, sell, and upgrade multiple Ezilis at once.\nIntended for use with my other mod SoloCoopMod.";
